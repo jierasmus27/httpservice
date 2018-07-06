a59576a49766ee05047ebcacb49aa3327c90be54
Jaco Erasmus <jaco.erasmus@mwrinfosecurity.com>
2018-07-06 16:59:03 +0200

    Add patch to update posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -29,6 +29,12 @@ export class PostsComponent {
     );
   }
 
+  updatePost(post) {
+    this.http.patch(this.url + "/" + post.id, JSON.stringify({isRead: true}))
+      .subscribe(response => {
+        console.log(response.json());
+      });
+  }
 
 
 }
