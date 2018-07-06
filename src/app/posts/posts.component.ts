ccb37b36c9b4c11855d729661fbb82c6201f9fc6
Jaco Erasmus <jaco.erasmus@mwrinfosecurity.com>
2018-07-06 16:52:07 +0200

    Add create posts method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -16,7 +16,18 @@ export class PostsComponent {
     });
   }
 
-  
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = "";
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+
+        this.posts.splice(0, 0, post);
+      }
+    );
+  }
 
 
 
